63604901392b7c27428c3de24decee688eb8bef4
Hunner4D <h.herrera.academics@gmail.com>
2020-06-05 14:32:00 -0700

    init

--- boilerplates/convert/index.js
+++ boilerplates/convert/index.js
@@ -1,46 +1,60 @@
-const electron = require('electron');
-const ffmpeg = require('fluent-ffmpeg');
-const _ = require('lodash');
-const { app, BrowserWindow, ipcMain } = electron;
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+const _ = require("lodash");
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            backgroundThrottling: false
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      backgroundThrottling: false,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
 
-ipcMain.on('videos:added', (event, videos) => {
-    const promises = _.map(videos, video => {
-        return new Promise((resolve, reject) => {
-            ffmpeg.ffprobe(video.path, (err, metadata) => {
-                resolve({
-                    ...video,
-                    duration: metadata.format.duration,
-                    format: 'avi'
-                });
-            });
+ipcMain.on("videos:added", (event, videos) => {
+  const promises = _.map(videos, (video) => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve({
+          ...video,
+          duration: metadata.format.duration,
+          format: "avi",
         });
+      });
     });
+  });
 
-    Promise.all(promises)
-        .then((results) => {
-            mainWindow.webContents.send('metadata:complete', results);
-        });
+  Promise.all(promises).then((results) => {
+    mainWindow.webContents.send("metadata:complete", results);
+  });
 });
 
-ipcMain.on('conversion:start', (event, videos) => {
-    const video = videos[0];
-
+ipcMain.on("conversion:start", (event, videos) => {
+  _.each(videos, (video) => {
     const outputDirectory = video.path.split(video.name)[0];
-    console.log(outputDirectory)
+    const outputName = video.name.split(".")[0];
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+    // console.log(outputDirectory);
+    // console.log(outputName);
+    // console.log(outputPath);
     ffmpeg(video.path)
-        .output()
+      .output(outputPath)
+      .on("progress", ({ timemark }) =>
+        mainWindow.webContents.send("conversion:progress", { video, timemark })
+      )
+      .on("end", () =>
+        mainWindow.webContents.send("conversion:end", { video, outputPath })
+      )
+      .run();
+  });
+});
+
+ipcMain.on("folder:open", (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
 });
